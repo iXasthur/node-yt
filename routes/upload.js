e1433e61245fd2791d00cc7bf61d2a736c25ebf3
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2021-02-07 18:23:44 +0300

    Update upload.js

--- routes/upload.js
+++ routes/upload.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    let head_title = 'Upload to node-yt'
+    let headTitle = 'Upload to node-yt'
 
     res.render('upload', {
-        head_title: head_title
+        head_title: headTitle
     });
 });
 
