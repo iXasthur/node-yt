0b0e463ea9814434a6dcc2f4b6e6a4ae51ac60e3
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2021-04-19 18:24:44 +0300

    Updated deps

--- app.js
+++ app.js
@@ -57,82 +57,8 @@ async function start() {
             console.log(`App was started on port ${PORT}`)
         })
 
-        const io = socket(server)
-        io.on('connection', (socket) => {
-            console.log('User connected with id = ' + socket.id)
 
-            socket.on('restore_auth', (data) => {
-                if (data) {
-                    let {jwt} = data
-                    if (verifyJwt(jwt)) {
-                        socket.emit('auth_result', { jwt })
-                    } else {
-                        socket.emit('auth_result', { error: 'Unable to verify provided jwt' })
-                    }
-                } else {
-                    socket.emit('auth_result', { error: 'Unable to verify provided jwt' })
-                }
-            })
 
-            socket.on('sign_up', async (data) => {
-                const {email, password} = data
-
-                if (await User.findOne({email})) {
-                    socket.emit('auth_result', { error: 'User with that email already exists' })
-                    return
-                }
-
-                const hashedPassword = await bcrypt.hash(password, 12)
-                const user = new User({
-                    email,
-                    hashedPassword
-                })
-
-                await user.save()
-
-                const jwt = createToken(user)
-
-                socket.emit('auth_result', { jwt })
-            })
-
-            socket.on('sign_in', async (data) => {
-                const {email, password} = data
-
-                const user = await User.findOne({email})
-                if (!user) {
-                    socket.emit('auth_result', { error: "User does not exist" })
-                    return
-                }
-
-                const isMatch = await bcrypt.compare(password, user.hashedPassword)
-                if (!isMatch) {
-                    socket.emit('auth_result', { error: "Invalid password" })
-                    return
-                }
-
-                const jwt = createToken(user)
-
-                socket.emit('auth_result', { jwt })
-            })
-
-            socket.on('get_videos_list', async (data) => {
-                if (data) {
-                    let {jwt} = data
-                    if (verifyJwt(jwt)) {
-                        let videos = await Video.find({})
-                        socket.emit('get_videos_list_result', { videos })
-                    } else {
-                        socket.emit('auth_result', { error: 'Unable to verify provided jwt' })
-                    }
-                } else {
-                    socket.emit('auth_result', { error: 'Unable to verify provided jwt' })
-                }
-            })
-
-            socket.on('disconnect', async () => {
-                console.log('User disconnected with id = ' + socket.id)
-            })
-        })
     } catch (e) {
         console.log(`Server error: ${e.message}`)
         process.exit(1)
